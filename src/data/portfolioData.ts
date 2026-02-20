17fe27c569c9ac27fcdc0d477d10cd636d7017c5
Bendraou Ayoub <bendraou.e2100818@etud.univ-ubs.fr>
2026-02-20 11:22:33 +0100

    Update new skills, projects and experience

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -2,16 +2,29 @@ import type { Experience, Education, Project, Certificate, SkillCategory } from 
 
 export const profileData = {
   name: "Ayoub BENDRAOU",
-  title: "Computer Science & Cybersecurity Engineering Student",
+  title: "Computer Science & Cybersecurity Engineer",
   email: "ayoub_bendraou@outlook.fr",
   phone: "+33 7.53.13.77.98",
   location: "Aubervilliers, France",
   github: "github.com/Youbey",
-  summary: "Cybersecurity & Computer Science engineering student at ENSIBS combining strong analytical skills with passion for digital security. Eager to contribute fresh perspectives and technical capabilities to a forward-thinking organization through a 6-month internship opportunity."
+  summary: "Cybersecurity & Computer Science Engineer combining strong analytical skills with passion for digital security. Eager to contribute fresh perspectives and technical capabilities to a forward-thinking organization."
 };
 
 export const experiences: Experience[] = [
   {
+    title: "Full-stack Developer & Project Lead Internship",
+    company: "ElecZy",
+    location: "Paris, France",
+    period: "03/2026 – 09/2026",
+    description: [
+        "End-to-End Product Development: Designed and developed a Decision Support System (DSS) application, managing the full software development life cycle (SDLC).",
+        "System Architecture & Strategy: Conducted requirements analysis and defined the technical architecture to ensure scalability and performance.",
+        "Full-Cycle Execution: Led the development, rigorous testing (V&V), and final deployment of the application",
+        "Security & Risk Management: Performed comprehensive risk assessments and implemented robust security protocols to protect sensitive data.",
+        "Web Management: Provided full-scale administration and maintenance for the corporate website, ensuring 100% uptime and functional updates & Security."
+    ]
+  },
+    {
     title: "Network & CyberSecurity Internship",
     company: "Palmarais Hospitality & Leisure",
     location: "Casablanca, Morocco",
@@ -35,11 +48,11 @@ export const experiences: Experience[] = [
   {
     title: "Development Internship",
     company: "Netia - DAVID SYSTEM",
-    location: "Montpellier, France",
+    location: "Munich, Germany",
     period: "04/2023 – 06/2023",
     description: [
-      "Web development and component development",
-      "Website management and security (WordPress)"
+      "Web & components development",
+      "Website management and security"
     ]
   },
   {
@@ -60,8 +73,7 @@ export const experiences: Experience[] = [
     period: "02/2022 – 06/2022",
     description: [
       "Developed an application with JavaFX / JavaSwing interface",
-      "Harmonized and centralized data",
-      "Worked as part of a team"
+      "Harmonized and centralized data"
     ]
   }
 ];
@@ -89,6 +101,40 @@ export const education: Education[] = [
 ];
 
 export const projects: Project[] = [
+    {
+        title: "Crisis Management Simulation & Operations Drill - GHL (Groupement Hositalier lanvaux)",
+        date: "02/2026",
+        description: "A simulation of a crisis management scenario involving a large hospital chain, due to a quality accident.",
+        details: [
+            "Rotational Leadership: Operated within a 3-unit structure (Management, Dev, and Ops/Maintenance), rotating roles daily to ensure cross-functional expertise and seamless handovers.\n" +
+            "\n" +
+            "Agile Product Delivery: Simultaneously managed new product development, deployment, and legacy updates while maintaining strict V&V (Validation & Verification) and quality standards.\n" +
+            "\n" +
+            "Cyber Incident Response: Acted as first responders to a major cyberattack and data breach involving social media leaks.\n" +
+            "\n" +
+            "    Technical: Identified breach sources and contrsucted the attack timeline with the attacker's motives and implemented a comprehensive remediation plan.\n" +
+            "\n" +
+            "    Regulatory & Legal: Handled CNIL declarations (GDPR compliance) and cooperated with Law Enforcement (Gendarmerie) investigations.\n" +
+            "\n" +
+            "Strategic Communication:\n" +
+            "\n" +
+            "    Managed high-stakes crisis comms with the CEO, the affected client, and the press.\n" +
+            "\n" +
+            "    Ensured business continuity and successfully negotiated the renewal of the client contract mid-crisis.\n" +
+            "\n" +
+            "Operational Excellence: Maintained detailed logs (main-courantes) for status tracking and prioritized tasks in a high-pressure, multi-cell environment.",
+        ]
+    },
+    {
+        title: "MLSecOpsResearch - IA Federated Learning",
+        date: "10/2025 - 03/2026",
+        description: "A cybersecurity research project on the application of secured environment for IA training using federated learning process",
+    },
+    {
+        title: "Sera - Automated Vulnerability Scanner",
+        date: "11/2025",
+        description: "Sera is an automated vulnerability scanning tool for web applications and APIs. It performs comprehensive security testing to identify common vulnerabilities including HTTP/HTTPS misconfigurations, SQL injection, XSS (Cross-Site Scripting), and integrates with OWASP ZAP for active and passive scanning.",
+    },
     {
         title: "defHack: Military Defensive CTF",
         date: "10/2025",
@@ -127,90 +173,94 @@ export const certificates: Certificate[] = [
 ];
 
 export const skillCategories: SkillCategory[] = [
-  {
-    category: "Application Development",
-    skills: [
-      "Java (Spring Boot/Maven/Gradle)",
-      "Jakarta EE/Payara",
-      "Python",
-      "Android & Security",
-      "Secure Distributed Systems",
-      "API Documentation & Testing with Swagger"
-    ]
-  },
-  {
-    category: "Network & Security Management",
-    skills: [
-      "Firewall",
-      "VPN (IPsec + SSL)",
-      "SSH",
-      "Reverse Proxy",
-      "Proxychains",
-      "HIDS/HIPS",
-      "pfSense",
-      "Wireshark"
-    ]
-  },
-  {
-    category: "Ethical Hacking",
-    skills: [
-      "Malware Analysis",
-      "Kali Linux/Parrot OS",
-      "OSINT",
-      "Metasploit",
-      "SQL Injection",
-      "Phishing",
-      "Nmap",
-      "Web Scraping"
-    ]
-  },
-  {
-    category: "Databases & Security",
-    skills: [
-      "SQL: MySQL, MariaDB, Oracle",
-      "NoSQL: MongoDB"
-    ]
-  },
+    {
+        category: "Software Architecture & Development",
+        skills: [
+            "Software Architecture & Security",
+            "Modeling (UML, Meta-modeling...)",
+            "Java (Spring Boot / Maven / Gradle)",
+            "Jakarta EE / Payara",
+            "Python",
+            "Secure Distributed Systems",
+            "API Documentation & Testing (Swagger)"
+        ]
+    },
+    {
+        category: "Network & Security Operations (SecOps)",
+        skills: [
+            "SIEM (Wazuh, ELK stack)",
+            "Firewall & VPN (IPsec + SSL)",
+            "HIDS/HIPS",
+            "Reverse Proxy & Proxychains",
+            "SSH",
+            "pfSense",
+            "Wireshark"
+        ]
+    },
+    {
+        category: "Ethical Hacking",
+        skills: [
+            "Malware Analysis",
+            "Kali Linux / Parrot OS",
+            "OSINT",
+            "Metasploit",
+            "SQL Injection",
+            "Phishing",
+            "Nmap",
+            "Web Scraping"
+        ]
+    },
+    {
+        category: "Governance, Risk & Strategy",
+        skills: [
+            "Governance, Risk & Compliance (GRC)",
+            "Business Strategy",
+            "System Qualifications (ISO, HDS...)",
+            "E-BIOS Risk Management",
+            "CyberEdu: Modules 1, 2, 3 & 4"
+        ]
+    },
+    {
+        category: "Operating Systems & System Security",
+        skills: [
+            "System Security",
+            "Windows & Linux",
+            "Windows Server / Wallix",
+            "Docker & Kubernetes",
+            "Lynis & DebScan"
+        ]
+    },
+    {
+        category: "Project Management & DevSecOps",
+        skills: [
+            "DevSecOps / DevOps",
+            "Ansible (Configuration Automation)",
+            "Jenkins (CI/CD)",
+            "Agile (Scrum / Kanban)",
+            "JIRA"
+        ]
+    },
+    {
+        category: "Databases & Web Development",
+        skills: [
+            "SQL (MySQL, MariaDB, Oracle)",
+            "NoSQL (MongoDB)",
+            "Web: React.js, Node.js, SCSS"
+        ]
+    },
   {
     category: "Web Development",
     skills: [
       "HTML/CSS/SCSS",
       "JavaScript",
-      "Node.js"
-    ]
-  },
-  {
-    category: "Risk Analysis",
-    skills: [
-      "Governance, Risk & Compliance (GRC)",
-      "E-BIOS RISK MANAGER",
-      "Lynis & DebScan (Linux Security)",
-      "CyberEdu: Modules 1, 2, 3 & 4"
-    ]
-  },
-  {
-    category: "Project Management",
-    skills: [
-      "DevSecOps / DevOps",
-      "Ansible (Configuration Automation)",
-      "Jenkins (CI/CD)",
-      "Scrum",
-      "Kanban",
-      "JIRA"
-    ]
-  },
-  {
-    category: "Operating Systems",
-    skills: [
-      "Windows - Linux",
-      "Windows Server / Wallix",
-      "Docker / Kubernetes"
+      "Node.js",
+      "React.js"
     ]
   }
 ];
 
 export const languages = [
-  { name: "French", level: "Delf B2" },
+  { name: "French", level: "Le Robert" },
   { name: "Arabic", level: "Native Language" },
   { name: "English", level: "TOEIC, ALC Certificate" }
 ];
@@ -218,5 +268,5 @@ export const languages = [
 export const interests = [
   { name: "Root-Me", detail: "Profile: Youbey" },
   { name: "Football", detail: "Position: Midfielder" },
-  { name: "Muy Thaï", detail: "Self-Taught" }
+  { name: "Muay Thaï", detail: "Self-Taught" }
 ];
